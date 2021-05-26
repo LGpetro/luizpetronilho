8ccc4af2779325187db6554339c3bc3ef6b30245
Luiz Petronilho <lglpspfc@gmail.com>
2021-05-26 16:48:13 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1622055631,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/rocket-sheet0.png",
+		"images/alien-sheet0.png",
+		"images/alien2-sheet0.png",
+		"images/p-sheet0.png",
+		"images/wall.png",
+		"images/tiledbackground2.png",
+		"images/f-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
